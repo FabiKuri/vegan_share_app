6d0d840ddb7458f97620da2e4a5835e1685c2099
FabiKuri <fabien.k.main@gmail.com>
2020-09-30 12:27:08 +0900

    activehash変更

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,20 +1,20 @@
 class Genre < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 4, name: 'Japanese Food' },
-    { id: 8, name: 'Asian Food' },
-    { id: 9, name: 'Italian' },
-    { id: 9, name: 'French' },
-    { id: 9, name: 'American' },
-    { id: 9, name: 'Indian' },
-    { id: 3, name: 'currey' },
-    { id: 9, name: 'Pasta' },
-    { id: 2, name: 'Pizza' },
-    { id: 6, name: 'Burger' },
-    { id: 6, name: 'Fast Food' },
-    { id: 7, name: 'Desserts' },
-    { id: 6, name: 'Cafe' },
-    { id: 7, name: 'Bakery' },
-    { id: 10, name: 'Other' }
+    { id: 0, name: '--' },
+    { id: 1, name: 'Japanese Food' },
+    { id: 2, name: 'Asian Food' },
+    { id: 3, name: 'Italian' },
+    { id: 4, name: 'French' },
+    { id: 5, name: 'American' },
+    { id: 6, name: 'Indian' },
+    { id: 7, name: 'currey' },
+    { id: 8, name: 'Pasta' },
+    { id: 9, name: 'Pizza' },
+    { id: 10, name: 'Burger' },
+    { id: 11, name: 'Fast Food' },
+    { id: 12, name: 'Desserts' },
+    { id: 13, name: 'Cafe' },
+    { id: 14, name: 'Bakery' },
+    { id: 15, name: 'Other' }
   ]
   end
