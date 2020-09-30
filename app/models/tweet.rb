305a4a4c7ff50fb5b3cf8e7350a46f1a7f34e39c
FabiKuri <fabien.k.main@gmail.com>
2020-09-30 17:00:58 +0900

    バリデーション

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,7 @@ class Tweet < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :genre
   belongs_to_active_hash :prefecture
+
+  validates :shop_name, :content, :genre_id, :plan_base_menu,
+  :prefecture_id, :food_menu, presence: true
 end
